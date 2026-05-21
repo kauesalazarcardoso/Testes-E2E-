97cb21d8cd7ad4540d59ebb6fe7518c894f2992b
Kauê Salazar <kauesalazarcardoso123@gmail.com>
2026-05-21 17:00:15 -0300

    Inicializa estrutura do projeto

---
+++ tests/login_test.py
@@ -0,0 +1,14 @@
+from pages.login_page import LoginPage
+
+def test_login_valido(driver):
+
+    login_page = LoginPage(driver)
+
+    login_page.open()
+
+    login_page.login(
+        "standard_user",
+        "secret_sauce"
+    )
+
+    assert "inventory" in driver.current_url
